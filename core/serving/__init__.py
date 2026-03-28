69c6fac3af5651ef5f72ad5440a0ca08e2858402
Morteza Taleblou <morteza_taleblou@yahoo.com>
2026-03-28 19:21:20 +0200

    Create __init__.py

---
+++ core/serving/__init__.py
@@ -0,0 +1,15 @@
+from .schemas import (
+    ServingBatchArtifactResponse,
+    ServingBatchJobResponse,
+    ServingBatchRunRequest,
+    ServingExplainResponse,
+    ServingPredictionResponse,
+)
+
+__all__ = [
+    "ServingBatchArtifactResponse",
+    "ServingBatchJobResponse",
+    "ServingBatchRunRequest",
+    "ServingExplainResponse",
+    "ServingPredictionResponse",
+]
