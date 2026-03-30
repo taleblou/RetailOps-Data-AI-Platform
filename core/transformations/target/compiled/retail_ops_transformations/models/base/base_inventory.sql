1448610d577064697e5574a4689e07e4ab10ffde
Morteza Taleblou <morteza_taleblou@yahoo.com>
2026-03-30 14:16:39 +0300

    remove

--- core/transformations/target/compiled/retail_ops_transformations/models/base/base_inventory.sql
+++
@@ -1,9 +0,0 @@
-select
-    store_id,
-    product_id,
-    snapshot_date,
-    on_hand_qty,
-    reserved_qty,
-    available_qty,
-    ingested_at
-from "retailops"."raw"."inventory_snapshots"
--- core/transformations/target/compiled/retail_ops_transformations/models/base/base_products.sql
+++
@@ -1,9 +0,0 @@
-select
-    product_id,
-    sku,
-    product_name,
-    category,
-    unit_price,
-    is_active,
-    ingested_at
-from "retailops"."raw"."products"
